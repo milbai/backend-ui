55b6a9931627a7a759e82fa8f7662b545169c1c5
miller.bai <miller.bai@kaiostech.com>
2021-02-10 11:41:46 +0800

    Init the project for backend ui.

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# backend-ui
+## 使用
+
+```bash
+$ git clone https://github.com/jetlinks/jetlinks-ui-antd.git
+$ cd jetlinks-ui-antd
+$ npm install
+$ npm start         # visit http://localhost:8000
+```
+>> 推荐使用淘宝镜像
+
+### 本地开发环境要求
+
+- nodeJs v12.x
+- npm v6.x
+- Chrome v80.0+
+
+本地开发项目建议使用如下命令启动项目
+
+```bash
+$ npm run start:dev     //支持：dev、test、pre环境
+```
+
+项目多处采用了 SSE 接口交互，开发需要使用 dev 环境变量（生产环境使用 nginx 代理了 EventSource 接口）
+
+### 修改后台接口地址
+
+后台接口配置文件地址：`config/proxy.ts`:
+
+```typescript
+/**
+ * 在生产环境 代理是无法生效的，所以这里没有生产环境的配置
+ */
+export default {
+  dev: {
+    '/jetlinks': {
+      target: '后台地址',
+      changeOrigin: true,
+      pathRewrite: { '^/jetlinks': '' },
+    },
+  },
+  test: {
+    '/jetlinks': {
+      target: '后台地址',
+      changeOrigin: true,
+      pathRewrite: { '^/jetlinks': '' },
+    },
+  },
+  pre: {
+    '/jetlinks': {
+      target: '后台地址',
+      changeOrigin: true,
+      pathRewrite: { '^/jetlinks': '' },
+    },
+  },
+};
+```
+
---
+++ src/pages/device/product/save/configuration.tsx
@@ -0,0 +1,165 @@
+import React, { useEffect, useState } from 'react';
+import { FormComponentProps } from 'antd/lib/form';
+import Form from 'antd/es/form';
+import { Button, Card, Col, Drawer, Icon, Input, Row, Select, Tooltip } from 'antd';
+import { DeviceProduct } from "@/pages/device/product/data";
+
+interface Props extends FormComponentProps {
+  data?: Partial<DeviceProduct>;
+  configuration?: any;
+  close: Function;
+  save: (data: Partial<DeviceProduct>) => void;
+}
+
+interface State {
+  protocolSupports: any[];
+  protocolTransports: any[];
+  organizationList: any[];
+  configForm: any[];
+  configName: string;
+}
+
+const Configuration: React.FC<Props> = props => {
+  const initState: State = {
+    protocolSupports: [],
+    protocolTransports: [],
+    organizationList: [],
+    configName: '',
+    configForm: [],
+  };
+
+  const { getFieldDecorator } = props.form;
+  // 配置名称
+  // const [configName, setConfigName] = useState(initState.configName);
+  // 配置表单
+  const [configForm, setConfigForm] = useState(initState.configForm);
+
+  const parseConfig = (configData: any[]) => {
+    const config = configData.map(item => {
+      const configName = item.name;
+      let properties = item.properties.map((i: any) => {
+        const label = i.description ? (
+          <span>
+            <span style={{marginRight: '10px'}}>{i.name}</span>
+            <Tooltip title={i.description}>
+              <Icon type="question-circle-o" />
+            </Tooltip>
+          </span>) : i.name
+        const key = `configuration.${i.property}`;
+        const componentType = i.type.id;
+        let component = null;
+        const options = {
+          initialValue: props.data?.configuration ? props.data?.configuration[i.property] : undefined,
+        };
+
+        if (componentType !== 'enum') {
+          component = componentType === 'password' ? <Input.Password /> : <Input type={'text'} />;
+        } else {
+          const o = i.type.elements;
+          component = (
+            <Select>
+              {(o || []).map((e: any) => (
+                <Select.Option key={e.value} value={e.value}>
+                  {e.text}
+                </Select.Option>
+              ))}
+            </Select>
+          );
+        }
+        return {
+          label,
+          key,
+          styles: {
+            xl: { span: 8 },
+            lg: { span: 8 },
+            md: { span: 12 },
+            sm: { span: 24 },
+          },
+          options,
+          component
+        };
+      })
+      return {
+        configName,
+        properties
+      }
+    });
+    setConfigForm(config);
+  };
+
+  useEffect(() => {
+    // setConfigName(props.configuration[0].name);
+    parseConfig(props.configuration)
+  }, []);
+
+
+  const saveData = () => {
+    const { form } = props;
+    form.validateFields((err, fileValue) => {
+      if (err) return;
+      props.save(fileValue);
+    });
+  };
+
+  return (
+    <Drawer
+      title='编辑配置'
+      visible
+      width={500}
+      onClose={() => props.close()}
+      closable
+    >
+      <Form labelCol={{ span: 6 }} wrapperCol={{ span: 18 }}>
+        {configForm && (
+          // <Card title="配置" style={{ marginBottom: 20 }} bordered={false}>
+            <Row gutter={16}>
+              {configForm.map((item, index) => (
+                <Col key={index}>
+                  <h4>{item.configName}</h4>
+                  {item.properties.map((i: any) => (
+                    <Col key={i.key}>
+                      <Form.Item label={i.label}>
+                        {getFieldDecorator(i.key, i.options)(i.component)}
+                      </Form.Item>
+                    </Col>
+                  ))}
+                </Col>
+              ))}
+            </Row>
+          // </Card>
+        )}
+      </Form>
+      <div
+        style={{
+          position: 'absolute',
+          right: 0,
+          bottom: 0,
+          width: '100%',
+          borderTop: '1px solid #e9e9e9',
+          padding: '10px 16px',
+          background: '#fff',
+          textAlign: 'right',
+        }}
+      >
+        <Button
+          onClick={() => {
+            props.close();
+          }}
+          style={{ marginRight: 8 }}
+        >
+          关闭
+                </Button>
+        <Button
+          onClick={() => {
+            saveData();
+          }}
+          type="primary"
+        >
+          保存
+                </Button>
+      </div>
+    </Drawer>
+  );
+};
+
+export default Form.create<Props>()(Configuration);
