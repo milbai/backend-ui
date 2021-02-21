5ea5f12e420e5e9e4ff87f87e6b380bdd7d55363
baishiwei <miller.bai@innolinks.cn>
2021-02-21 23:19:18 +0800

    Add the users menu

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 docker build -t registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-antd:1.0-SNAPSHOT .
 docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-antd:1.0-SNAPSHOT
+
+
+docker login --username=songll1688 registry.cn-beijing.aliyuncs.com
+docker tag [ImageId] registry.cn-beijing.aliyuncs.com/rwslinks/rwslinks-ui:latest
+docker push registry.cn-beijing.aliyuncs.com/rwslinks/rwslinks-ui:latest
