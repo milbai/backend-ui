faeaa7ae0efb28e33ef57ed02fb11067e851d1af
baishiwei <miller.bai@innolinks.cn>
2021-02-23 21:23:26 +0800

    update docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -6,3 +6,5 @@ docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-antd:1.0-SNAP
 docker login --username=songll1688 registry.cn-beijing.aliyuncs.com
 docker tag [ImageId] registry.cn-beijing.aliyuncs.com/rwslinks/rwslinks-ui:latest
 docker push registry.cn-beijing.aliyuncs.com/rwslinks/rwslinks-ui:latest
+
+docker build -t registry.cn-beijing.aliyuncs.com/rwslinks/rwslinks-ui:latest
