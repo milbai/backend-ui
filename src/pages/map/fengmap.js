d57c9d15e96b0cb62463471e43d85a00e739a169
bailei <bai51555@163.com>
2021-05-15 19:36:06 +0800

    地图位置信息显示

---
+++ src/pages/map/fengmap.js
@@ -0,0 +1,114 @@
+import fengmap from 'fengmap';
+var map;
+var fenceList, cm100List;
+
+export function createFengmap(callback, setCurrentItem) {
+  var fmapID = '1384053067182067713';
+  var mapOptions = {
+    container: document.getElementById('fengmap'),
+    mapServerURL: './fengmap/data/' + fmapID,
+    mapThemeURL: './fengmap/data/theme',
+    defaultThemeName: '3b91d03288204d02368dd4f68fc1f189',
+    mapScaleLevelRange: [16, 23],       // 比例尺级别范围， 16级到23级
+    // mapScaleRange: [200, 4000]      // 自定义比例尺范围，单位（厘米）
+    defaultMapScaleLevel: 17,          // 默认比例尺级别设置为19级
+    appName: '陈头岗地铁停车场',
+    key: '40308d481d2d806bcd2e5fb346c2dc45',
+  };
+  map = new fengmap.FMMap(mapOptions);
+  map.openMapById(fmapID, function (error) {
+    console.log(error);
+  });
+  map.on('loadComplete', callback);
+
+  map.on('mapClickNode', function (event) {
+    //console.log(event);
+    var nodeType = event.nodeType;
+    var target = event.target;
+    if(!nodeType || !target || (nodeType !== 36 && nodeType !== 31)) {
+      setCurrentItem({});
+      return;
+    }
+    if(nodeType === 36) {
+      setCurrentItem(fenceList[target.index]);
+    } else if(nodeType === 31) {
+      setTimeout(function () {
+        setCurrentItem(cm100List[target.index]);
+      }, 0);
+    }
+  });
+}
+
+export function addPolygonMarker(data) {
+  fenceList = data;
+  //console.log(data);
+  const fenceCoords = [
+    //月检线
+    [
+      { x: 12609343.487486389, y: 2634680.7467382923, z: 56 },
+      { x: 12609339.252967019, y: 2634651.1668030284, z: 56 },
+      { x: 12609706.621351019, y: 2634652.1526199896, z: 56 },
+      { x: 12609706.650074564, y: 2634681.1130687636,  z: 56 },
+    ],
+    //维修线
+    [
+      { x: 12609257.503689926, y: 2634651.916090568, z: 56 },
+      { x: 12609258.790374342, y: 2634636.405447829, z: 56 },
+      { x: 12609707.527558396, y: 2634634.2928042985, z: 56 },
+      { x: 12609706.033594407, y: 2634652.290200657, z: 56 },
+    ],
+    //停车日检库A区
+    [
+      { x: 12609242.913614508, y: 2634621.3513399083, z: 56 },
+      { x: 12609239.662802674, y: 2634466.6577441064, z: 56 },
+      { x: 12609613.856840886, y: 2634465.754081998, z: 56 },
+      { x: 12609613.544191122, y: 2634621.7920239493, z: 56 },
+    ],
+    //停车日检库B区
+    [
+      { x: 12609647.203478605, y: 2634622.74148204, z: 56 },
+      { x: 12609645.15114645, y: 2634464.339464341, z: 56 },
+      { x: 12609998.237355687, y: 2634466.8285023263, z: 56 },
+      { x: 12610002.36026345, y: 2634620.025391266, z: 56 },
+    ]
+  ];
+  for(var i = 0; i < fenceCoords.length; i++) {
+    if(data[i]) {
+      addPolygonMarker(fenceCoords[i], i);
+    }
+  }
+  function addPolygonMarker(coords, index) {
+    var group = map.getFMGroup(map.focusGroupID);
+    var layer = group.getOrCreateLayer('polygonMarker');
+    var polygonMarker = new fengmap.FMPolygonMarker({
+      alpha: .8,             //设置透明度
+      color: '#CD5A5A',
+      lineWidth: 0,      //设置边框线的宽度
+      height: 6,    //设置高度*/
+      points: coords //多边形坐标点
+    });
+    polygonMarker.index = index;
+    layer.addMarker(polygonMarker);
+  }
+}
+
+export function updateMarkers(data) {
+  var group = map.getFMGroup(map.focusGroupID);
+  var layer = group.getOrCreateLayer('imageMarker');
+  layer.removeAll();
+  //console.log(data);
+  cm100List = data;
+  for(var i = 0; i < data.length; i++) {
+    var im = new fengmap.FMImageMarker({
+      x: parseFloat(data[i].longitude),
+      y: parseFloat(data[i].latitude),
+      url: './fengmap/images/blueImageMarker.png',
+      //设置图片显示尺寸
+      size: 32,
+      //标注高度，大于model的高度
+      height: 4
+    });
+    im.index = i;
+    layer.addMarker(im);
+  }
+}
--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import {Form} from "antd";
+import {Divider, Form} from "antd";
 import {FormComponentProps} from "antd/lib/form";
-import Fengmap from "./Fengmap";
 import {useEffect} from "react";
 import {defer, from} from "rxjs";
 import apis from "@/services";
@@ -10,93 +9,150 @@ import {filter, map} from "rxjs/operators";
 import {useState} from "react";
 import moment from "moment";
 
+import { createFengmap, addPolygonMarker, updateMarkers } from './fengmap'
+import styles from './css/index.css';
+
 interface Props extends FormComponentProps {
 }
 
 interface State {
-  saveVisible: boolean;
+  currentItem: any;
 }
 
 const Location: React.FC<Props> = props => {
-  const [fenceList, setFenceList] = useState<any[]>([]);
-
   const initState: State = {
-    saveVisible: false
+    currentItem: {}
   };
-
-  const [saveVisible, setSaveVisible] = useState(initState.saveVisible);
+  const [currentItem, setCurrentItem] = useState(initState.currentItem);
 
   useEffect(() => {
-    defer(
-      () => from(apis.employee.listAll(encodeQueryParam({ terms: {type: 1} }))).pipe(
-        filter(resp => resp.status === 200),
-        map(resp => resp.result)
-      )).subscribe((data) => {
-      const tempEmployee = {};
+    var requestData;
+    createFengmap(mapDone, setCurrentItem);
 
-      data.map((item: any) => (tempEmployee[item.id] = item.name));
-      console.log(tempEmployee);
-
-      function getEmployeeName(employees) {
-        if(!employees)
-          return '';
-        var emp = employees.split(',');
-        var result = '';
-        for(var j = 0; j < emp.length; j++) {
-          result += tempEmployee[emp[j]] + ' ';
-        }
-        return result;
-      }
+    function mapDone() {
+      //console.log('地图加载完成！');
       defer(
-        () => from(apis.fence.listAll(encodeQueryParam({}))).pipe(
+        () => from(apis.employee.listAll(encodeQueryParam({ terms: {type: 1} }))).pipe(
           filter(resp => resp.status === 200),
           map(resp => resp.result)
         )).subscribe((data) => {
-        const tempFence = data.map((item: any) => ({
-          name: item.name,
-          area: item.area,
-          begin: item.begin ? moment(item.begin).format('YYYY-MM-DD HH:mm:ss') : '',
-          end: item.end ? moment(item.end).format('YYYY-MM-DD HH:mm:ss') : '',
-          status: item.begin && item.end && item.begin < moment().valueOf() && moment().valueOf() < item.end,
-          outsideAlarm: item.outsideAlarm ? '开启' : '关闭',
-          outsideTimeout: item.outsideTimeout + '分钟',
-          insideAlarm: item.insideAlarm ? '开启' : '关闭',
-          insideTimeout: item.insideTimeout + '分钟',
-          employees: getEmployeeName(item.employees)
-        }));
-        var temp = [];
-        for(var i = 0; i < tempFence.length; i++) {
-          if(temp[0] && temp[1] && temp[2] && temp[3]) {
-            break;
+        const tempEmployee = {};
+        data.map((item: any) => (tempEmployee[item.id] = item.name));
+        //console.log(tempEmployee);
+
+        function getEmployeeName(employees) {
+          if(!employees)
+            return '';
+          var emp = employees.split(',');
+          var result = '';
+          for(var j = 0; j < emp.length; j++) {
+            result += tempEmployee[emp[j]] + ' ';
           }
-          if(!temp[0] && tempFence[i].area === '月检线' && tempFence[i].status) {
-            temp[0] = tempFence[i];
-            continue;
-          }
-          if(!temp[1] && tempFence[i].area === '维修线' && tempFence[i].status) {
-            temp[1] = tempFence[i];
-            continue;
-          }
-          if(!temp[2] && tempFence[i].area === '停车日检库A区' && tempFence[i].status) {
-            temp[2] = tempFence[i];
-            continue;
-          }
-          if(!temp[3] && tempFence[i].area === '停车日检库B区' && tempFence[i].status) {
-            temp[3] = tempFence[i];
-          }
+          return result;
         }
-        setFenceList(temp);
-        setSaveVisible(true);
+        defer(
+          () => from(apis.fence.listAll(encodeQueryParam({}))).pipe(
+            filter(resp => resp.status === 200),
+            map(resp => resp.result)
+          )).subscribe((data) => {
+          const tempFence = data.map((item: any) => ({
+            name: item.name,
+            area: item.area,
+            begin: item.begin ? moment(item.begin).format('YYYY-MM-DD HH:mm:ss') : '',
+            end: item.end ? moment(item.end).format('YYYY-MM-DD HH:mm:ss') : '',
+            status: item.begin && item.end && item.begin < moment().valueOf() && moment().valueOf() < item.end,
+            outsideAlarm: item.outsideAlarm ? '开启' : '关闭',
+            outsideTimeout: item.outsideTimeout + '分钟',
+            insideAlarm: item.insideAlarm ? '开启' : '关闭',
+            insideTimeout: item.insideTimeout + '分钟',
+            employees: getEmployeeName(item.employees)
+          }));
+          var temp = [];
+          for(var i = 0; i < tempFence.length; i++) {
+            if(temp[0] && temp[1] && temp[2] && temp[3]) {
+              break;
+            }
+            if(!temp[0] && tempFence[i].area === '月检线' && tempFence[i].status) {
+              temp[0] = tempFence[i];
+              continue;
+            }
+            if(!temp[1] && tempFence[i].area === '维修线' && tempFence[i].status) {
+              temp[1] = tempFence[i];
+              continue;
+            }
+            if(!temp[2] && tempFence[i].area === '停车日检库A区' && tempFence[i].status) {
+              temp[2] = tempFence[i];
+              continue;
+            }
+            if(!temp[3] && tempFence[i].area === '停车日检库B区' && tempFence[i].status) {
+              temp[3] = tempFence[i];
+            }
+          }
+          addPolygonMarker(temp);
+        });
       });
-    });
+
+      const getData = () => {
+        //蓝牙胸卡 CM100
+        defer(
+          () => from(apis.deviceInstance.listAll(encodeQueryParam({ terms: {productId: 'CM100'} }))).pipe(
+            filter(resp => resp.status === 200),
+            map(resp => resp.result)
+          )).subscribe((data) => {
+          const tempCm100 = data.filter((item: any) => {
+            item.createTime = item.createTime ? moment(item.createTime).format('YYYY-MM-DD HH:mm:ss') : '';
+            item.registryTime = item.registryTime ? moment(item.registryTime).format('YYYY-MM-DD HH:mm:ss') : '';
+            return item.state && item.state.value === 'online';
+          });
+          updateMarkers(tempCm100);
+        });
+      };
+      getData();
+      requestData = setInterval(() => getData(), 3000);
+    }
+    return () => {
+      clearInterval(requestData);
+    };
   }, []);
 
-  return (
-    saveVisible &&
-    <Fengmap
-      data={fenceList}
-    />
-  )
+  return <div>
+    <div className={styles.mapout}>
+      <div className={styles.fengMap} id="fengmap"></div>
+      <div className={styles.mapmask}></div>
+      {currentItem.productId === "CM100" && (
+        <div id="cm100_modal" className={styles.fenceModal}>
+          产品名称<span className={styles.vRight}>{currentItem.productName}</span>
+          <Divider className={styles.fengge} />
+          设备名称<span className={styles.vRight}>{currentItem.name}</span>
+          <Divider className={styles.fengge} />
+          创建时间<span className={styles.vRight}>{currentItem.createTime}</span>
+          <Divider className={styles.fengge} />
+          注册时间<span className={styles.vRight}>{currentItem.registryTime}</span>
+        </div>
+      )}
+      {currentItem.area && (
+        <div id="fence_modal" className={styles.fenceModal}>
+          名称<span className={styles.vRight}>{currentItem.name}</span>
+          <Divider className={styles.fengge} />
+          区域<span className={styles.vRight}>{currentItem.area}</span>
+          <Divider className={styles.fengge} />
+          开始时间<span className={styles.vRight}>{currentItem.begin}</span>
+          <Divider className={styles.fengge} />
+          结束时间<span className={styles.vRight}>{currentItem.end}</span>
+          <Divider className={styles.fengge} />
+          离开报警<span className={styles.vRight}>{currentItem.outsideAlarm}</span>
+          <Divider className={styles.fengge} />
+          离开超时<span className={styles.vRight}>{currentItem.outsideTimeout}</span>
+          <Divider className={styles.fengge} />
+          未进入报警<span className={styles.vRight}>{currentItem.insideAlarm}</span>
+          <Divider className={styles.fengge} />
+          进入超时<span className={styles.vRight}>{currentItem.insideTimeout}</span>
+          <Divider className={styles.fengge} />
+          可进入员工<span className={styles.vRight}>{currentItem.employees}</span>
+        </div>
+      )}
+    </div>
+  </div>
 };
 
 export default Form.create<Props>()(Location);
