b016e0962f9522163e0ce256f41eda5f5d8da9a4
baishiwei <miller.bai@innolinks.cn>
2021-07-18 13:43:41 +0800

    更新地图，移除没监控的区域。

--- src/pages/map/fengmap.js
+++ src/pages/map/fengmap.js
@@ -10,17 +10,18 @@ export function createFengmap(callback, setCurrentItem, getTGSG_state) {
   var fmapID = '1384053067182067713';
   var mapOptions = {
     container: document.getElementById('fengmap'),
-    //mapServerURL: './fengmap/data/' + fmapID,
-    //mapThemeURL: './fengmap/data/theme',
+    mapServerURL: './fengmap/data/' + fmapID,
+    mapThemeURL: './fengmap/data/theme',
     defaultThemeName: '3b91d03288204d02368dd4f68fc1f189',
     mapScaleLevelRange: [16, 23],       // 比例尺级别范围， 16级到23级
     // mapScaleRange: [200, 4000]      // 自定义比例尺范围，单位（厘米）
-    defaultMapScaleLevel: 17,          // 默认比例尺级别设置为19级
+    defaultMapScaleLevel: 18,          // 默认比例尺级别设置为19级
     appName: '陈头岗地铁停车场',
     key: '40308d481d2d806bcd2e5fb346c2dc45',
     // 把地图转成俯视图的效果
     defaultViewMode: fengmap.FMViewMode.MODE_2D,
     defaultControlsPose: 0,     //角度值。
+    defaultViewCenter: { x: 12609603.113274425, y: 2634539.7186213997 },
   };
   map = new fengmap.FMMap(mapOptions);
   map.openMapById(fmapID, function (error) {
